c5d3422e52cf4e01849f816546cef670566e1ae5
Atsuki Demizu <demmys.atsuki@gmail.com>
2014-05-29 23:37:14 +0900

    文字コードを適切に設定

--- contact/contact.php
+++ contact/contact.php
@@ -5,12 +5,12 @@
 
     $to = 'hokan@sfc.keio.ac.jp';
     $subject = 'Webフォームからのお問い合わせ';
-    $body = 'お名前: '.$name.'\n\nメールアドレス: '.$email.'\n\nお問い合せ内容: \n'.$message;
+    $body = 'お名前: '.$name."\n\nメールアドレス: ".$email."\n\nお問い合せ内容: \n".$message;
     $header = 'From:'.$email;
 
     mb_language('japanese');
-    mb_internal_encoding('EUC-JP');
-    mail($to, $subject, $body, $header);
+    mb_internal_encoding('UTF-8');
+    mb_send_mail($to, $subject, $body, $header);
 
     header('Location: index.html');
 ?>
