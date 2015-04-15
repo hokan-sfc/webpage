93e046f4e39583183c63d3f9a1475aa226229def
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-04-15 17:53:21 +0900

    テスト用環境を分け、DBのテストができるように設定を変更

--- lib/internal/config/config_default.php
+++ lib/internal/config/config_default.php
@@ -1,17 +1,24 @@
 <?php
 
 return array(
-    'db' => array(
-        'file' => '/db/database.sqlite3'
+    'default' => array(
+        'db' => array(
+            'file' => '/db/database.data'
+        ),
+        'yahoo' => array(
+            'client_id' => '',
+            'client_secret' => '',
+            'callback' => 'http://hokan.sfc.keio.ac.jp/member/yahoo_login.php'
+        ),
+        'google' => array(
+            'client_id' => '',
+            'client_secret' => ''
+        )
     ),
-    'yahoo' => array(
-        'client_id' => '',
-        'client_secret' => '',
-        'callback' => 'http://hokan.sfc.keio.ac.jp/member/yahoo_login.php'
-    ),
-    'google' => array(
-        'client_id' => '',
-        'client_secret' => ''
+    'test' => array(
+        'db' => array(
+            'file' => '/db/test_database.data'
+        )
     )
 );
 
--- lib/internal/config/config_handler.php
+++ lib/internal/config/config_handler.php
@@ -7,26 +7,44 @@ use YConnect\YConnectClient;
 
 class Config {
     private $c;
+    private $env;
 
     function __construct() {
         $this->c = include(__DIR__.'/config.php');
+        $this->env = getenv('PHP_ENV');
+        if (!$this->env) {
+            $this->env = 'default';
+        }
+    }
+
+    private function db_file() {
+        return $this->c[$this->env]['db']['file'];
+    }
+
+    function sqlite() {
+        if ($this->env == 'test') {
+            $path = '.'.$this->db_file();
+        } else {
+            $path = $_SERVER['DOCUMENT_ROOT'].$this->db_file();
+        }
+        return "sqlite:$path";
     }
 
     function yahoo_client_id() {
-        return $this->c['yahoo']['client_id'];
+        return $this->c[$this->env]['yahoo']['client_id'];
     }
 
     function yahoo_callback() {
-        return $this->c['yahoo']['callback'];
+        return $this->c[$this->env]['yahoo']['callback'];
     }
 
     function google_client_id() {
-        return $this->c['google']['client_id'];
+        return $this->c[$this->env]['google']['client_id'];
     }
 
     function yahoo_client() {
-        $app = $this->c['yahoo']['client_id'];
-        $sec = $this->c['yahoo']['client_secret'];
+        $app = $this->c[$this->env]['yahoo']['client_id'];
+        $sec = $this->c[$this->env]['yahoo']['client_secret'];
         $cred = new ClientCredential($app, $sec);
         return new YConnectClient($cred);
     }
